ca089d6e3ec08b3ec1a682ed9e18445bff4fb8d1
Dimitar <dimitar.mandarliev@gmail.com>
2024-05-29 11:10:30 +0300

    Transaction hooks

--- app/api/[[...route]]/transactions.ts
+++ app/api/[[...route]]/transactions.ts
@@ -12,6 +12,7 @@ import { zValidator } from "@hono/zod-validator";
 import { createId } from "@paralleldrive/cuid2";
 import { string, z } from "zod";
 import { parse, subDays } from "date-fns";
+import { useAuth } from "@clerk/nextjs";
 
 const app = new Hono()
   .get(
@@ -135,6 +136,37 @@ const app = new Hono()
           id: createId(),
           ...values,
         })
+        .returning();
+
+      return c.json({ data });
+    }
+  )
+  .post(
+    "/bulk-create",
+    clerkMiddleware(),
+    zValidator(
+      "json",
+      z.array(
+        insertTransactionSchema.omit({
+          id: true,
+        })
+      )
+    ),
+    async (c) => {
+      const auth = getAuth(c);
+      const values = c.req.valid("json");
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const data = await db
+        .insert(transactions)
+        .values(
+          values.map((value) => ({
+            id: createId(),
+            ...value,
+          }))
+        )
         .returning();
 
       return c.json({ data });
