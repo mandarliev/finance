61bf0c68c161ce6ff07590c0739e5536d56bcbb1
Dimitar <dimitar.mandarliev@gmail.com>
2024-06-04 15:15:24 +0300

    Settings page

---
+++ app/(dashboard)/settings/settings-card.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { cn } from "@/lib/utils";
+
+const SettingsCard = () => {
+  const connectedBank = null;
+  return (
+    <Card className="border-none drop-shadow-sm ">
+      <CardHeader>
+        <CardTitle className="text-xl line-clamp-1">Settings</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Separator />
+        <div className="flex flex-col gap-y-2 lg:flex-row items-center py-4">
+          <p className="text-sm font-medium w-full lg:w-[16.5rem]">
+            Bank cccount
+          </p>
+          <div className="w-full flex items-center justify-between">
+            <div
+              className={cn(
+                "text-sm truncate flex items-center",
+                !connectedBank && "text-muted-foreground"
+              )}
+            >
+              {connectedBank
+                ? "Bank account connected"
+                : "No bank account connected"}
+            </div>
+            <Button size="sm" variant="ghost">
+              Connect
+            </Button>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default SettingsCard;
